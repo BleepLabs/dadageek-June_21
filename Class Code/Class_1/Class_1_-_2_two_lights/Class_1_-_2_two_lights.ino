e28ce15096b4d45bbe59529d18b2bd55547813fc
Dr. Bleep <drbleep@bleeplabs.com>
2021-06-29 20:27:31 -0500

    Update Class_1_-_2_two_lights.ino

--- Class Code/Class_1/Class_1_-_2_two_lights/Class_1_-_2_two_lights.ino
+++ Class Code/Class_1/Class_1_-_2_two_lights/Class_1_-_2_two_lights.ino
@@ -3,6 +3,7 @@ int led2_output;
 long current_time;
 long prev_time;
 long prev_time2;
+int button_read;
 
 void setup() {
   pinMode(5, INPUT_PULLUP);
@@ -13,33 +14,44 @@ void setup() {
 void loop() {
   current_time = millis();
 
-  if (current_time - prev_time2 > 100) {
-    prev_time2 = current_time;
+  button_read = digitalRead(5); //0 when button is pressed
 
-    if (led2_output == 0) {
-      led2_output = 1;
-    }
-    else {
-      led2_output = 0;
-    }    
+  if (button_read == 0) {
 
-    digitalWrite(10, led2_output);
+    if (current_time - prev_time2 > 100) {
+      prev_time2 = current_time;
 
+      if (led2_output == 0) {
+        led2_output = 1;
+      }
+      else {
+        led2_output = 0;
+      }
+
+      digitalWrite(10, led2_output);
+
+    } //end of led2 timing if
+
+    if (current_time - prev_time > 500) {
+      prev_time = current_time;
+
+      if (led1_output == 0) {
+        led1_output = 1;
+      }
+      else {
+        led1_output = 0;
+      }
+      Serial.println(led1_output);
+
+      digitalWrite(9, led1_output);
+
+    } //end of led1 timing if
+
+  } //end of button if
+  
+  else {
+    digitalWrite(9, 0);
+    digitalWrite(10, 0);
   }
 
-  if (current_time - prev_time > 500) {
-    prev_time = current_time;
-
-    if (led1_output == 0) {
-      led1_output = 1;
-    }
-    else {
-      led1_output = 0;
-    }    
-    Serial.println(led1_output);    
-    
-    digitalWrite(9, led1_output);
-
-  }
-
-}
+} //end of loop
